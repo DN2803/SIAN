b926c37c932ea9def42aace4b03a813ef87982aa
DN2803 <ntkngann2k3@gmail.com>
2025-06-05 20:36:38 +0900

    model CGAN

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import sys
 import data
 from options.train_option import TrainOptions
+from utils.iter_counter import IterationCounter
+from utils.visualizer import Visualizer
+from trainers.pix2pix_trainer import Pix2PixTrainer
+
 # parse options
 opt = TrainOptions().parse()
 
@@ -8,5 +12,14 @@ opt = TrainOptions().parse()
 print(' '.join(sys.argv))
 
 # load the dataset
-# dataloader = data.create_dataloader(opt)
+dataloader = data.create_dataloader(opt)
+
+# create trainer for our model
+trainer = Pix2PixTrainer(opt)
+
+# create tool for counting iterations
+iter_counter = IterationCounter(opt, len(dataloader))
+
+# create tool for visualization
+visualizer = Visualizer(opt)
 
