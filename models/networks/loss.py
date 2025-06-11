63a6dbbaa5ca4e0b4c0f7020ff7fafde57b08a7f
DN2803 <ntkngann2k3@gmail.com>
2025-06-11 17:36:00 +0900

    fix

--- models/networks/loss.py
+++ models/networks/loss.py
@@ -100,7 +100,7 @@ class GANLoss(nn.Module):
 class VGGLoss(nn.Module):
     def __init__(self, gpu_ids):
         super(VGGLoss, self).__init__()
-        self.vgg = VGG19().cuda()
+        self.vgg = VGG19().cuda().half()
         self.criterion = nn.L1Loss()
         self.weights = [1.0 / 32, 1.0 / 16, 1.0 / 8, 1.0 / 4, 1.0]
 
