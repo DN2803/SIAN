9fa0e446fc5f0c20b97c0128d45b1900a15ad6df
DN2803 <ntkngann2k3@gmail.com>
2025-06-17 11:11:21 +0700

    fix

--- data/cropcleavageembryov_dataset.py
+++ data/cropcleavageembryov_dataset.py
@@ -61,10 +61,10 @@ class CropcleavageEmbryovDataset(Pix2pixDataset):
         direction_map = self.resize_map(direction_path)
         image = np.load(image_path)
         # ressize ảnh về kích thước chuẩn
-        image = np.transpose(image, (2, 0, 1))  # chuyển từ (H, W, C) sang (C, H, W)
+        
         if self.opt.load_size > 0:
             image = cv2.resize(image, (self.opt.load_size, self.opt.load_size), interpolation=cv2.INTER_LINEAR)
-        
+        image = np.transpose(image, (2, 0, 1))  # chuyển từ (H, W, C) sang (C, H, W)
         image = torch.from_numpy(image).float()  # chuyển sang tensor float
         
 
